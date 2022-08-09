01b21af7dae2a1d42caa563dc1e027380eabf6e6
isabelanery <isabelanery242@gmail.com>
2022-08-09 19:46:27 -0300

    feat: abstract class Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,21 @@
-export default class Race {
+export default abstract class Race {
+  constructor(private readonly _name: string, private readonly _dexterity: number) {
+    // this.name = _name;
+    // this.dexterity = _dexterity;
+  }
 
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    const error = new Error('Not implemented');
+    throw error;
+  }
+
+  abstract get maxLifePoints(): number
 }
