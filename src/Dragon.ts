d89502683dd54f391e9813bdd0816488b0df528f
isabelanery <isabelanery242@gmail.com>
2022-08-10 17:31:19 -0300

    feat: class Dragon

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -1,5 +1,8 @@
-import Monster from "./Monster";
+import Monster from './Monster';
 
 export default class Dragon extends Monster {
-  
+  constructor() {
+    super();
+    super._lifePoints = 999;
+  }
 }
